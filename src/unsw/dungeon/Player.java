b73e57243eccc356b624c8613f38e15467b31444
Benjamin Mickan <benjaminmickan@dodo.com.au>
2020-07-13 15:56:52 +1000

    Walls done, exit shows up on map but does nothing at the moment

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -19,23 +19,49 @@ public class Player extends Entity {
         this.dungeon = dungeon;
     }
 
+    // For each valid movement, we check if the player is on a wall, if it is, push it back.
     public void moveUp() {
-        if (getY() > 0)
+        if (getY() > 0) {
             y().set(getY() - 1);
+            if (isOnWall())
+                y().set(getY() + 1);
+        }
     }
 
     public void moveDown() {
-        if (getY() < dungeon.getHeight() - 1)
+        if (getY() < dungeon.getHeight() - 1) {
             y().set(getY() + 1);
+            if (isOnWall())
+                y().set(getY() - 1);
+        }
     }
 
     public void moveLeft() {
-        if (getX() > 0)
+        if (getX() > 0) {
             x().set(getX() - 1);
+            if (isOnWall())
+                x().set(getX() + 1);
+        }
     }
 
     public void moveRight() {
-        if (getX() < dungeon.getWidth() - 1)
+        if (getX() < dungeon.getWidth() - 1) {
             x().set(getX() + 1);
+            if (isOnWall())
+                x().set(getX() - 1);
+        }
+    }
+
+    private boolean isOnWall() {
+        for (Entity e : dungeon.getEntities()) {
+            // This is probably bad... hopefully we can make this class more dynamic
+            // maybe not isOnWall, but isOnEntity, then it can be reused for weapons and other things.
+            if (e instanceof Wall) {
+                if (getX() == e.getX() && getY() == e.getY()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
