5b7d3df40caea290e2607393bb066af62c0b2d9b
Sihang <z5223840@ad.unsw.edu.au>
2020-07-21 00:25:03 +1000

    some unused imports are deleted

--- src/test/BoulderTest.java
+++ src/test/BoulderTest.java
@@ -1,37 +1,19 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.FileNotFoundException;
 
 import org.junit.jupiter.api.Test;
 
-
-import java.io.File;
 import java.io.IOException;
 
-import org.junit.jupiter.api.Test;
-
-import unsw.dungeon.DungeonController;
 import unsw.dungeon.Boulder;
-import unsw.dungeon.Door;
 import unsw.dungeon.Dungeon;
-import unsw.dungeon.DungeonControllerLoader;
-import unsw.dungeon.DungeonLoader;
-import unsw.dungeon.Enemy;
-import unsw.dungeon.Entity;
-import unsw.dungeon.Exit;
 import unsw.dungeon.Goal;
 import unsw.dungeon.Player;
 import unsw.dungeon.Portal;
-import unsw.dungeon.Potion;
 import unsw.dungeon.Switch;
-import unsw.dungeon.Treasure;
 import unsw.dungeon.Wall;
-import unsw.dungeon.Weapon;
-import unsw.dungeon.Key;
+
 
 public class BoulderTest {
     
