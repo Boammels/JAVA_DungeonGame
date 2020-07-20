8d81f6c718d748700658095e741b4cc2bfd4b9b4
Sihang <z5223840@ad.unsw.edu.au>
2020-07-20 19:09:09 +1000

    some tests finished

--- src/test/TestJunit.java
+++ src/test/TestJunit.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileNotFoundException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,31 +32,30 @@ public class TestJunit {
     @Test
     public void playerMove() {
 
-        Dungeon dungeon = new Dungeon(5,5);
-        Player player = new Player(dungeon, 1,2);
+        Dungeon dungeon = new Dungeon(5, 5);
+        Player player = new Player(dungeon, 1, 2);
         Goal goal = new Goal("treasure");
         dungeon.addEntity(player);
         dungeon.addGoal(goal);
-        for(int i=0; i < 4; i++) {
-            Wall wall = new Wall(0,i);
-            Wall wall2 = new Wall (3,i);
+        for (int i = 0; i < 4; i++) {
+            Wall wall = new Wall(0, i);
+            Wall wall2 = new Wall(3, i);
             dungeon.addEntity(wall);
             dungeon.addEntity(wall2);
         }
-        for(int i=1;i<3;i++) {
-            Wall wall = new Wall(i,0);
-            Wall wall2 = new Wall (i,3);
+        for (int i = 1; i < 3; i++) {
+            Wall wall = new Wall(i, 0);
+            Wall wall2 = new Wall(i, 3);
             dungeon.addEntity(wall);
             dungeon.addEntity(wall2);
         }
-        Treasure treasure = new Treasure(dungeon, 4,4);
+        Treasure treasure = new Treasure(dungeon, 4, 4);
         dungeon.addEntity(treasure);
 
-        //remove above if below works
-        /** 
-        TestDungeonLoader load = new TestDungeonLoader("movementTest.json");
-        Dungeon dungeon = load.load();
-        Player player = dungeon.getPlayer();
+        // remove above if below works
+        /**
+         * TestDungeonLoader load = new TestDungeonLoader("movementTest.json"); Dungeon
+         * dungeon = load.load(); Player player = dungeon.getPlayer();
          */
         player.moveRight();
         assertEquals(player.getX(), 2);
@@ -81,21 +82,21 @@ public class TestJunit {
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 2);
     }
-    
+
     @Test
     public void KeyDoorPortalTreasure() {
-        Dungeon dungeon = new Dungeon(7,4);
+        Dungeon dungeon = new Dungeon(7, 4);
         Goal goal = new Goal("treasure");
         dungeon.addGoal(goal);
-        for(int i = 0; i < 7; i++) {
-            Wall wall = new Wall(i,0);
-            Wall wall2 = new Wall (i,3);
+        for (int i = 0; i < 7; i++) {
+            Wall wall = new Wall(i, 0);
+            Wall wall2 = new Wall(i, 3);
             dungeon.addEntity(wall);
             dungeon.addEntity(wall2);
         }
-        for(int i = 1; i < 3; i++) {
-            Wall wall = new Wall(0,i);
-            Wall wall2 = new Wall (6,i);
+        for (int i = 1; i < 3; i++) {
+            Wall wall = new Wall(0, i);
+            Wall wall2 = new Wall(6, i);
             dungeon.addEntity(wall);
             dungeon.addEntity(wall2);
         }
@@ -105,10 +106,12 @@ public class TestJunit {
         dungeon.addEntity(player);
         dungeon.addEntity(key);
         dungeon.addEntity(door);
-        Portal portal1 = new Portal(dungeon,3,2);
+        Portal portal1 = new Portal(dungeon, 3, 2);
         dungeon.addEntity(portal1);
-        
-        Treasure treasure = new Treasure(dungeon,5,2);
+        Portal portal2 = new Portal(dungeon, 5, 1);
+        dungeon.addEntity(portal2);
+
+        Treasure treasure = new Treasure(dungeon, 5, 2);
         dungeon.addEntity(treasure);
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 1);
@@ -118,27 +121,52 @@ public class TestJunit {
         player.moveDown();
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 2);
-        assert(!dungeon.getEntities().contains(key));
-        assertEquals(player.getKey(),1);
+        assert (!dungeon.getEntities().contains(key));
+        assertEquals(player.getKey(), 1);
         player.moveUp();
         player.moveRight();
-        assert(door.isOpen());
+        assert (door.isOpen());
         assertEquals(player.getX(), 2);
         assertEquals(player.getY(), 1);
         player.moveRight();
         player.moveDown();
-        assertEquals(player.getX(), 3);
-        assertEquals(player.getY(), 2);
-        Portal portal2 = new Portal(dungeon,5,1);
-        dungeon.addEntity(portal2);
-        player.moveUp();
-        player.moveDown();
         assertEquals(player.getX(), 5);
         assertEquals(player.getY(), 1);
         player.moveDown();
         assertEquals(player.getX(), 5);
         assertEquals(player.getY(), 2);
-        assert(!dungeon.getEntities().contains(treasure));
-        assert(goal.getGoalComplete());
+        assert (!dungeon.getEntities().contains(treasure));
+        assert (goal.getGoalComplete());
     }
+
+    @Test
+    public void EnemyTest1() throws InterruptedException {
+        Dungeon dungeon = new Dungeon(3, 1);
+        Goal goal = new Goal("enemy");
+        dungeon.addGoal(goal);
+
+        Player player = new Player(dungeon, 0, 0);
+        Enemy enemy = new Enemy(dungeon, player, 2, 0);
+        dungeon.addEntity(player);
+        dungeon.addEntity(enemy);
+        Timer timer = new Timer();
+        Thread.sleep(3000);
+        assertEquals(dungeon.getState(), "PlayerDead");
+        /*assertEquals(enemy.getX(), 1);
+        assertEquals(enemy.getY(), 0);
+        
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+                assertEquals(enemy.getX(), 1);
+                assertEquals(enemy.getY(), 0);
+                
+                timer.cancel();
+            }
+        },2000);
+
+
+        */
+    }
+
 }
