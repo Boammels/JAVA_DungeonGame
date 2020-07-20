f215302cd79ce9f6a8fe67b0abce77df79ecc7c7
Sihang <z5223840@ad.unsw.edu.au>
2020-07-20 19:42:44 +1000

    some tests done with json

--- src/test/TestJunit1.java
+++ src/test/TestJunit1.java
@@ -21,6 +21,7 @@ import unsw.dungeon.Dungeon;
 import unsw.dungeon.DungeonControllerLoader;
 import unsw.dungeon.DungeonLoader;
 import unsw.dungeon.Enemy;
+import unsw.dungeon.Entity;
 import unsw.dungeon.Exit;
 import unsw.dungeon.Goal;
 import unsw.dungeon.Player;
@@ -71,37 +72,52 @@ public class TestJunit1 {
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 2);
 
-
-        
-
-
-
-
     }
 
     @Test
-    public void testWalls() throws IOException {
+    public void testTools() throws IOException {
         String currentDirectory = System.getProperty("user.dir");
         System.out.println(currentDirectory);
-        TestDungeonLoader load = new TestDungeonLoader(currentDirectory + "/bin/test/maze.json");
+        TestDungeonLoader load = new TestDungeonLoader(currentDirectory + "/bin/test/keyDoorTreasurePortal.json");
         Dungeon dungeon = load.load();
         System.out.println(dungeon.getEntities().size());
         Player player = dungeon.getPlayer();
         System.out.println(player);
 
+        Key key = null;
+        Door door = null;
+        Treasure treasure = null;
+        for (Entity e : dungeon.getEntities()) {
+            if(e instanceof Key) {
+                key = (Key)e;
+            } else if (e instanceof Door) {
+                door = (Door)e;
+            } else if (e instanceof Treasure) {
+                treasure = (Treasure)e;
+            }
+        }
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 1);
         player.moveRight();
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 1);
-        player.moveLeft();
-        assertEquals(player.getX(), 1);
-        assertEquals(player.getY(), 1);
-        player.moveUp();
-        assertEquals(player.getX(), 1);
-        assertEquals(player.getY(), 1);
         player.moveDown();
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 2);
+        assert (!dungeon.getEntities().contains(key));
+        assertEquals(player.getKey(), 1);
+        player.moveUp();
+        player.moveRight();
+        assert (door.isOpen());
+        assertEquals(player.getX(), 2);
+        assertEquals(player.getY(), 1);
+        player.moveRight();
+        player.moveDown();
+        assertEquals(player.getX(), 5);
+        assertEquals(player.getY(), 1);
+        player.moveDown();
+        assertEquals(player.getX(), 5);
+        assertEquals(player.getY(), 2);
+        assert (!dungeon.getEntities().contains(treasure));
     }
 }
